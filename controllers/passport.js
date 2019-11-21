4d7ee137d428660cf407c7a2c898e25951dffad7
Claudio Rivetta <c.rivetta@veryroadtrip.eu>
2019-11-21 11:36:35 +0100

    fixed bug error msg in passport

--- controllers/passport.js
+++ controllers/passport.js
@@ -12,46 +12,44 @@ var JwtStrategy = require('passport-jwt').Strategy,
 
 
 passport.use(
-  'local-signup',
-  new LocalStrategy(
-    {
-      usernameField: 'email',
-      passwordField: 'password',
-      passReqToCallback: true,
-      session: false,
-    },
-    (req, email, password, done) => {
-      console.log(username);
-      console.log("rbemail:",req.body.email,"email:", email);
+    'local-signup',
+    new LocalStrategy(
+        {
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true,
+        session: false,
+        },
+        (req, email, password, done) => {
+            db.one('SELECT * FROM account WHERE email = $1', email.toLowerCase())
+            .then(users => {
+                if (users.length) {
+                    console.log('username or email already taken');
+                    return done(null, false, {
+                    message: 'username or email already taken',
+                    });
+                }
+                bcrypt.hash(password, BCRYPT_SALT_ROUNDS).then(hashedPassword => {
+                    let newUser = {
+                        password: hashedPassword,
+                        email: email.toLowerCase(),
+                    }
+                    db.any('INSERT INTO account (email,password) VALUES($1:csv) RETURNING id;',newUser).then( row => {
+                        newUser.id = row.id; //Last Id Insert
+                        console.log('user created');
+                        return done(null, newUser);
+                    }).catch((error) => {
+                        console.error('problem to create user', error);
+                        return done(null, false, { message: 'problem to create user' });
+                    });
+                });
+            }).catch((error) => {
+                console.error('problem to create user', error);
+                return done(null, false, { message: 'bad username / email' });
+            });
 
-      try {
-        let sql = `SELECT * FROM account WHERE email = '${email.toLowerCase()}' limit 1`;
-        db.any(sql)
-        .then(users => {
-          if (users.length) {
-            console.log('username or email already taken');
-            return done(null, false, {
-              message: 'username or email already taken',
-            });
-          }
-          bcrypt.hash(password, BCRYPT_SALT_ROUNDS).then(hashedPassword => {
-            let newUser = {
-              password: hashedPassword,
-              email: email.toLowerCase(),
-            }
-            db.any('INSERT INTO account (email,password) VALUES($1:csv) RETURNING id;',newUser).then( row => {
-              newUser.id = row.id; //Last Id Insert
-              console.log('user created');
-              return done(null, newUser);
-            });
-          });
-        });
-      } catch (error) {
-        console.log(error);
-        return done(error);
-      }
-    },
-  ),
+        }
+    )
 );
 
 passport.use(
@@ -63,25 +61,23 @@ passport.use(
       passReqToCallback: true
     },
     function (req, email, password, done) {
-      try {
-			db.one('SELECT * FROM account WHERE email = $1',email.toLowerCase())
-			.then(user => {
-				if (user === null) {
-					return done(null, false, { message: 'bad username / email' });
-				}
-				bcrypt.compare(password, user.password).then(response => {
-					if (response !== true) {
-						console.log('passwords do not match');
-						return done(null, false, { message: 'passwords do not match' });
-					}
-					console.log('user found & authenticated');
-					return done(null, user);
-				});
-			});
-      } catch (err) {
-        console.log("Error:", err);
-        return done(null,false,{message: err});
-      }
+        db.one('SELECT * FROM account WHERE email = $1',email.toLowerCase())
+        .then(user => {
+          if (user === null) {
+            return done(null, false, { message: 'bad username / email' });
+          }
+          bcrypt.compare(password, user.password).then(response => {
+            if (response !== true) {
+              console.log('passwords do not match');
+              return done(null, false, { message: 'passwords do not match' });
+            }
+            console.log('user found & authenticated');
+            return done(null, user);
+          });
+        }).catch((error) => {
+          console.error('bad username / email');
+          return done(null, false, { message: 'bad username / email' });
+        });
     },
   ),
 );
