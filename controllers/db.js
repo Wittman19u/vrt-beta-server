75f96e337e575cbd810384f3113a638adcd8fdfb
VeryRoadTrip <c.rivetta@veryroadtrip.eu>
2020-04-21 18:33:18 +0200

    add vars db

--- controllers/db.js
+++ controllers/db.js
@@ -49,12 +49,12 @@ const pgp = require('pg-promise')(pgPromiseOptions);
 
 const db = pgp({
 	// CONFIG
-	// host: process.env.DB_HOST, // server name or IP address;
-	// port: process.env.DB_PORT,
-	// database: process.env.DB_DATABASE,
-	// user: process.env.DB_USER,
-	// ssl: process.env.DB_SSL ,
-	// password: process.env.DB_PASSWORD,
+	host: process.env.DB_HOST, // server name or IP address;
+	port: process.env.DB_PORT,
+	database: process.env.DB_DATABASE,
+	user: process.env.DB_USER,
+	ssl: process.env.DB_SSL ,
+	password: process.env.DB_PASSWORD,
 	uri: process.env.DB_URI
 }); // database instance;
 
