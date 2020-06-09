c95d8e457cb8cd59494e3f11dfabc30fed49c579
Wittman19u <antoine.wittmann@hotmail.fr>
2020-06-09 15:51:54 +0200

    add delete alert route + limit/offset on get

--- controllers/alerts.js
+++ controllers/alerts.js
@@ -2,6 +2,8 @@ const db = require('./db');
 const passport = require('passport');
 
 function getUserAlerts(req, res, next) {
+	var limit = parseParam(req.query.limit, 10)
+	var offset = parseParam(req.query.offset, 0)
 	passport.authenticate('jwt', { session: false },function (error, user, info) {
 		if (user === false || error || info !== undefined) {
 			let message = {
@@ -16,7 +18,7 @@ function getUserAlerts(req, res, next) {
 			console.error(message);
 			res.status(403).json(message);
 		} else {
-			db.any('SELECT * FROM alert WHERE recipient_id = $1 ORDER BY isread, id DESC', user.id).then(function (alerts) {
+			db.any('SELECT * FROM alert WHERE recipient_id = $1 ORDER BY isread, id DESC LIMIT $2 OFFSET $3', [user.id, limit, offset]).then(function (alerts) {
 				res.status(200).json({
 					status: 'success',
 					data: alerts,
@@ -32,6 +34,45 @@ function getUserAlerts(req, res, next) {
 	})(req, res, next);
 }
 
+function deleteAlert(req, res, next) {
+	passport.authenticate('jwt', { session: false },function (error, user, info) {
+		if (user === false || error || info !== undefined) {
+			let message = {
+				status: 'error',
+				error: error,
+				user: user
+			};
+			if(info !== undefined){
+				message['message']= info.message;
+				message['info']= info;
+			}
+			console.error(message);
+			res.status(403).json(message);
+		} else {
+			var alert_id = parseInt(req.params.id)
+			db.none('DELETE FROM alert WHERE id = $1 AND recipient_id = $2', [alert_id, user.id]).then(function () {
+				res.status(200).json({
+					status: 'Success',
+					message: `Succesfully deleted alert ${alert_id}`
+				})
+			}).catch(function (err) {
+				res.status(500).json({
+					status: 'error',
+					message: `Problem during remove db (alerts): ${err}`
+				})
+			});
+		}
+	})(req, res, next);
+}
+
+// to parse limit/offset/etc... (any optional int parameters)
+function parseParam(param, defaultValue) {
+	const parsed = parseInt(param);
+	if (isNaN(parsed)) { return defaultValue; }
+	return parsed;
+}
+
 module.exports = {
-	getUserAlerts: getUserAlerts
+	getUserAlerts: getUserAlerts,
+	deleteAlert: deleteAlert
 };
--- routes/alerts.js
+++ routes/alerts.js
@@ -35,6 +35,19 @@ var router = express.Router();
  *       - application/json
  *     security:
  *       - authorisationJWT: []
+ *     parameters:
+ *       - name: limit
+ *         description: Limit of results returned
+ *         in: query
+ *         default: 10
+ *         required: false
+ *         type: integer
+ *       - name: offset
+ *         description: Offset in db
+ *         in: query
+ *         default: 0
+ *         required: false
+ *         type: integer
  *     responses:
  *       200:
  *         description: A list of alerts.
@@ -44,5 +57,32 @@ var router = express.Router();
  *             $ref: '#/definitions/Alert'
  */
 router.get('/user', alertController.getUserAlerts);
+
+/**
+ * @swagger
+ * /api/alerts/{id}:
+ *   delete:
+ *     tags:
+ *       - Alerts
+ *     description: Deletes a single alert
+ *     produces:
+ *       - application/json
+ *     security:
+ *       - authorisationJWT: []
+ *     parameters:
+ *       - name: id
+ *         description: Alert's id
+ *         in: path
+ *         required: true
+ *         type: integer
+ *     responses:
+ *       200:
+ *         description: Successfully deleted
+ *       403:
+ *         description: The user is not authenticated
+ *       500:
+ *         description: DB/login error
+ */
+router.delete('/:id', alertController.deleteAlert);
 
 module.exports = router;
